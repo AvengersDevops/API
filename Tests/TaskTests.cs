965a76597b0bdc6a45ea0413d0aa9803e698299d
Alvaro Luca Riccardi Roca de Togores <101105168+Lucarrdt@users.noreply.github.com>
2023-04-27 16:39:01 +0200

    More unit tests

--- Tests/TaskTests.cs
+++ Tests/TaskTests.cs
@@ -144,4 +144,132 @@ public class TaskTests
         Assert.That(response, Is.Not.Empty);
         Assert.That(response["status"]!.ToString() == "error", Is.True);
     }
+    [Test]
+    public async Task ReadGood()
+    {
+        var body = new Dictionary<string, string>
+        {
+            { "id", "1" }
+        };
+
+        var json = JsonConvert.SerializeObject(body);
+        var request = new DefaultHttpContext
+        {
+            Request =
+            {
+                Body = new MemoryStream(Encoding.UTF8.GetBytes(json))
+            }
+        };
+        
+        var taskController = new TaskController
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = request
+            }
+        };
+        
+        var r = JsonConvert.SerializeObject(await taskController.Read());
+        var response = JObject.Parse(r);
+        
+        Assert.That(response, Is.Not.Null);
+        Assert.That(response, Is.Not.Empty);
+        Assert.That(response["status"]!.ToString() == "success", Is.True);
+    }
+    [Test]
+    public async Task ReadBad()
+    {
+        var body = new Dictionary<string, string>
+        {
+            { "id", "1" }
+        };
+
+        var json = JsonConvert.SerializeObject(body);
+        var request = new DefaultHttpContext
+        {
+            Request =
+            {
+                Body = new MemoryStream(Encoding.UTF8.GetBytes(json))
+            }
+        };
+        
+        var taskController = new TaskController
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = request
+            }
+        };
+        
+        var r = JsonConvert.SerializeObject(await taskController.Read());
+        var response = JObject.Parse(r);
+        
+        Assert.That(response, Is.Not.Null);
+        Assert.That(response, Is.Not.Empty);
+        Assert.That(response["status"]!.ToString() == "error", Is.True);
+    }
+    [Test]
+    public async Task ReadAllGood()
+    {
+        var body = new Dictionary<string, string>
+        {
+            { "userId", "1" }
+        };
+
+        var json = JsonConvert.SerializeObject(body);
+        var request = new DefaultHttpContext
+        {
+            Request =
+            {
+                Body = new MemoryStream(Encoding.UTF8.GetBytes(json))
+            }
+        };
+        
+        var taskController = new TaskController
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = request
+            }
+        };
+        
+        var r = JsonConvert.SerializeObject(await taskController.ReadAll());
+        var response = JObject.Parse(r);
+        
+        Assert.That(response, Is.Not.Null);
+        Assert.That(response, Is.Not.Empty);
+        Assert.That(response["status"]!.ToString() == "success", Is.True);
+    }
+    [Test]
+    public async Task ReadAllBad()
+    {
+        var body = new Dictionary<string, string>
+        {
+            { "userId", "1" }
+        };
+
+        var json = JsonConvert.SerializeObject(body);
+        var request = new DefaultHttpContext
+        {
+            Request =
+            {
+                Body = new MemoryStream(Encoding.UTF8.GetBytes(json))
+            }
+        };
+        
+        var taskController = new TaskController
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = request
+            }
+        };
+        
+        var r = JsonConvert.SerializeObject(await taskController.ReadAll());
+        var response = JObject.Parse(r);
+        
+        Assert.That(response, Is.Not.Null);
+        Assert.That(response, Is.Not.Empty);
+        Assert.That(response["status"]!.ToString() == "error", Is.True);
+    }
 }
