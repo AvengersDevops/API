ea7fa41cbba6d26d5eda8efb7241b4865eb1b3c5
Alvaro Luca Riccardi Roca de Togores <101105168+Lucarrdt@users.noreply.github.com>
2023-04-28 18:52:16 +0200

    UserRequest.cs create, remove, edit

---
+++ AvengersAPI/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using AvengersAPI.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvengersAPI.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class UserController
+{
+    private readonly MyDbContext _context = new();
+    
+    [HttpPost]
+    public async 
+}
---
+++ AvengersAPI/Foo/UserRequest.cs
@@ -0,0 +1,86 @@
+﻿using AvengersAPI.Models;
+using Task = AvengersAPI.Entities.User;
+
+namespace AvengersAPI.Foo;
+
+public abstract class UserRequest
+{
+    public static Task? Create(dynamic body, out CustomResponse? customResponse)
+    {
+        if (body.name is null)
+        {
+            customResponse = new CustomResponse("error", "Name is null");
+            return null;
+        }
+
+        if (body.email is null)
+        {
+            customResponse = new CustomResponse("error", "Email is null");
+            return null;
+        }
+
+        if (body.password is null)
+        {
+            customResponse = new CustomResponse("error", "Password is null");
+            return null;
+        }
+        
+        customResponse = null;
+        return new Task
+        {
+            Name = body.name,
+            Email = body.email,
+            Password = body.password
+        };
+    }
+
+    public static Task? Delete(dynamic body, out CustomResponse? customResponse)
+    {
+        if (body.id is null)
+        {
+            customResponse = new CustomResponse("error", "Id is null");
+            return null;
+        }
+
+        customResponse = null;
+        
+        return new Task
+        {
+            Id = int.Parse(body.id.ToString())
+        };
+    }
+
+    public static Task? Update(dynamic body, out CustomResponse? customResponse)
+    {
+        if (body.id is null)
+        {
+            customResponse = new CustomResponse("error", "Id is null");
+            return null;
+        }
+
+        if (body.name is null)
+        {
+            customResponse = new CustomResponse("error", "Name is null");
+            return null;
+        }
+        if (body.email is null)
+        {
+            customResponse = new CustomResponse("error", "Email is null");
+            return null;
+        }
+        if (body.password is null)
+        {
+            customResponse = new CustomResponse("error", "Password is null");
+            return null;
+        }
+
+        customResponse = null;
+        return new Task
+        {
+            Id = int.Parse(body.id.ToString()),
+            Name = body.name,
+            Email = body.email,
+            Password = body.password
+        };
+    }
+}
