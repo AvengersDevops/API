ab61da250faecccdf0b81b0b569d1f680514b11e
Alvaro Luca Riccardi Roca de Togores <101105168+Lucarrdt@users.noreply.github.com>
2023-04-28 19:00:07 +0200

    create UserController.cs

--- AvengersAPI/Controllers/UserController.cs
+++ AvengersAPI/Controllers/UserController.cs
@@ -1,13 +1,27 @@
 ﻿using AvengersAPI.Context;
+using AvengersAPI.Entities;
+using AvengersAPI.Foo;
+using AvengersAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AvengersAPI.Controllers;
 [ApiController]
 [Route("[controller]")]
-public class UserController
+public class UserController : ControllerBase
 {
     private readonly MyDbContext _context = new();
-    
+
     [HttpPost]
-    public async 
+    public async Task<CustomResponse> Create()
+    {
+        CustomResponse? customResponse = null;
+        User? user = await Validator.Body<User>(Request.Body, d => UserRequest.Create(d, out customResponse));
+        if (customResponse is not null)
+            return customResponse;
+        
+        await _context.Users.AddAsync(user!);
+        await _context.SaveChangesAsync();
+        
+        return new CustomResponse("success", $"User created", user);
+    }
 }
