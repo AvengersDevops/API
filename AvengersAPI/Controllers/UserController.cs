8d6060a73baef3ced398e4589a2819ced0b0acdc
Alvaro Luca Riccardi Roca de Togores <101105168+Lucarrdt@users.noreply.github.com>
2023-04-28 19:03:47 +0200

    UserController update, delete and create

--- AvengersAPI/Controllers/UserController.cs
+++ AvengersAPI/Controllers/UserController.cs
@@ -24,4 +24,41 @@ public class UserController : ControllerBase
         
         return new CustomResponse("success", $"User created", user);
     }
+    [HttpPost]
+    public async Task<CustomResponse> Delete()
+    {
+        CustomResponse? customResponse = null;
+        User? user = await Validator.Body<User>(Request.Body, d => UserRequest.Delete(d, out customResponse));
+        if (customResponse is not null)
+            return customResponse;
+        
+        var u = await _context.Users.FindAsync(user!.Id);
+        if (u is null)
+            return new CustomResponse("error", $"User with id {user.Id} not found");
+        
+        _context.Users.Remove(u);
+        await _context.SaveChangesAsync();
+        
+        return new CustomResponse("success", $"User with id {user.Id} deleted");
+    }
+
+    [HttpPost]
+    public async Task<CustomResponse> Update()
+    {
+        CustomResponse? customResponse = null;
+        User? user = await Validator.Body<User>(Request.Body, d => UserRequest.Update(d, out customResponse));
+        if (customResponse is not null)
+            return customResponse;
+        
+        var u = await _context.Users.FindAsync(user!.Id);
+        if (u is null)
+            return new CustomResponse("error", $"User with id {user.Id} not found");
+        
+        u.Name = user.Name;
+        u.Email = user.Email;
+        u.Password = user.Password;
+        await _context.SaveChangesAsync();
+        
+        return new CustomResponse("success", $"User with id {user.Id} updated", u);
+    }
 }
