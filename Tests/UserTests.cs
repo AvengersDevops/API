0aa481de8d2c56406aafa283e3f05bd5e4650463
marpond <sonkoly.marci02@gmail.com>
2023-05-10 17:25:19 +0200

    improved code coverage

--- Tests/UserTests.cs
+++ Tests/UserTests.cs
@@ -21,22 +21,36 @@ public class UserTests
             { "email", "Testemail" },
             { "password", "Testpassword" }
         };
+        
         UserController userController = new();
         
-        await TestHandler.Do(body, Expect.Success, userController, userController.Create);
+        await TestHandler.Run(body, Expect.Success, userController, userController.Create);
     }
     
     [Test]
     public async Task CreateBad()
     {
-        var body = new Dictionary<string, dynamic>
+        var nullName = new Dictionary<string, dynamic>
+        {
+            { "email", "Testemail" },
+            { "password", "Testpassword" }
+        };
+        var nullEmail = new Dictionary<string, dynamic>
         {
             { "name", "Testname" },
             { "password", "Testpassword" }
         };
+        var nullPassword = new Dictionary<string, dynamic>
+        {
+            { "name", "Testname" },
+            { "email", "Testemail" }
+        };
+        
         UserController userController = new();
         
-        await TestHandler.Do(body, Expect.Error, userController, userController.Create);
+        await TestHandler.Run(nullName, Expect.Error, userController, userController.Create);
+        await TestHandler.Run(nullEmail, Expect.Error, userController, userController.Create);
+        await TestHandler.Run(nullPassword, Expect.Error, userController, userController.Create);
     }
 
     [Test]
@@ -46,21 +60,25 @@ public class UserTests
         {
             { "id", "1" }
         };
+        
         UserController userController = new();
         
-        await TestHandler.Do(body, Expect.Success, userController, userController.Read);
+        await TestHandler.Run(body, Expect.Success, userController, userController.Read);
     }
     
     [Test]
     public async Task ReadBad()
     {
-        var body = new Dictionary<string, dynamic>
+        var nullId = new Dictionary<string, dynamic>();
+        var badId = new Dictionary<string, dynamic>
         {
             { "id", "0" }
         };
+        
         UserController userController = new();
         
-        await TestHandler.Do(body, Expect.Error, userController, userController.Read);
+        await TestHandler.Run(nullId, Expect.Error, userController, userController.Read);
+        await TestHandler.Run(badId, Expect.Error, userController, userController.Read);
     }
     
     [Test]
@@ -73,24 +91,47 @@ public class UserTests
             { "email", "Testemail" },
             { "password", "Testpassword" }
         };
+        
         UserController userController = new();
         
-        await TestHandler.Do(body, Expect.Success, userController, userController.Update);
+        await TestHandler.Run(body, Expect.Success, userController, userController.Update);
     }
     
     [Test]
     public async Task UpdateBad()
     {
-        var body = new Dictionary<string, dynamic>
+        var badId = new Dictionary<string, dynamic>
         {
             { "id", "0" },
             { "name", "Testname" },
             { "email", "Testemail" },
             { "password", "Testpassword" }
         };
+        var nullId = new Dictionary<string, dynamic>
+        {
+            { "name", "Testname" },
+            { "email", "Testemail" },
+            { "password", "Testpassword" }
+        };
+        var nullName = new Dictionary<string, dynamic>
+        {
+            { "id", "1" },
+            { "email", "Testemail" },
+            { "password", "Testpassword" }
+        };
+        var nullEmail = new Dictionary<string, dynamic>
+        {
+            { "id", "1" },
+            { "name", "Testname" },
+            { "password", "Testpassword" }
+        };
+        
         UserController userController = new();
         
-        await TestHandler.Do(body, Expect.Error, userController, userController.Update);
+        await TestHandler.Run(badId, Expect.Error, userController, userController.Update);
+        await TestHandler.Run(nullId, Expect.Error, userController, userController.Update);
+        await TestHandler.Run(nullName, Expect.Error, userController, userController.Update);
+        await TestHandler.Run(nullEmail, Expect.Error, userController, userController.Update);
     }
     
     [Test]
@@ -102,21 +143,25 @@ public class UserTests
         {
             { "id", maxId.ToString() }
         };
+        
         UserController userController = new();
         
-        await TestHandler.Do(body, Expect.Success, userController, userController.Delete);
+        await TestHandler.Run(body, Expect.Success, userController, userController.Delete);
     }
 
     [Test]
     public async Task DeleteBad()
     {
-        var body = new Dictionary<string, dynamic>
+        var badId = new Dictionary<string, dynamic>
         {
             { "id", "0" }
         };
+        var nullId = new Dictionary<string, dynamic>();
+        
         UserController userController = new();
         
-        await TestHandler.Do(body, Expect.Error, userController, userController.Delete);
+        await TestHandler.Run(badId, Expect.Error, userController, userController.Delete);
+        await TestHandler.Run(nullId, Expect.Error, userController, userController.Delete);
     }
 
     [Test]
@@ -127,21 +172,39 @@ public class UserTests
             { "email", "Testemail" },
             { "password", "Testpassword" }
         };
+        
         UserController userController = new();
         
-        await TestHandler.Do(body, Expect.Success, userController, userController.Login);
+        await TestHandler.Run(body, Expect.Success, userController, userController.Login);
     }
     
     [Test]
     public async Task LoginBad()
     {
-        var body = new Dictionary<string, dynamic>
+        var badEmail = new Dictionary<string, dynamic>
+        {
+            { "email", "AAAAAAAA" },
+            { "password", "Testpassword" }
+        };
+        var badPassword = new Dictionary<string, dynamic>
         {
             { "email", "Testemail" },
             { "password", "AAAAAAAAA" }
         };
+        var nullEmail = new Dictionary<string, dynamic>
+        {
+            { "password", "AAAAAAAAA" }
+        };
+        var nullPassword = new Dictionary<string, dynamic>
+        {
+            { "email", "Testemail" }
+        };
+        
         UserController userController = new();
         
-        await TestHandler.Do(body, Expect.Error, userController, userController.Login);
+        await TestHandler.Run(badEmail, Expect.Error, userController, userController.Login);
+        await TestHandler.Run(badPassword, Expect.Error, userController, userController.Login);
+        await TestHandler.Run(nullEmail, Expect.Error, userController, userController.Login);
+        await TestHandler.Run(nullPassword, Expect.Error, userController, userController.Login);
     }
 }
