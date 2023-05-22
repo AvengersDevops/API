07a6f7637ea379b4d0179fee4718db4150748b8b
Marpond <89220481+Marpond@users.noreply.github.com>
2023-05-22 14:58:50 +0200

    Create K6SoakTest.js

---
+++ Tests/K6SoakTest.js
@@ -0,0 +1,32 @@
+import http from 'k6/http'
+import { check } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export const options = {
+  thresholds: {
+    http_req_failed: ['rate<0.01'],
+  },
+};
+
+export const stages = [
+  { duration: '20s', target: 400 },
+  { duration: '20s', target: 400 },
+  { duration: '20s', target: 0 },
+];
+
+export default function () {
+    const data = { id: 1 };
+    let res = http.post(`${__ENV.HOSTING}/user/read`, JSON.stringify(data),
+        {
+            headers: { 'Content-Type': 'application/json' }
+        }
+    );
+    const checkSuccess = check(res, { 'User Read Success': (r) => r.status === 200 });
+    if (!checkSuccess) fail('User Read Failed');
+}
+
+export function handleSummary(data) {
+    return {
+        "/Tests/K6Reports/LoadTestSummary.html": htmlReport(data),
+    };
+}
