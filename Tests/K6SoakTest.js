9498d3eb30a090c4baf8de2691261eb56f3c0bc4
Marpond <89220481+Marpond@users.noreply.github.com>
2023-05-22 15:06:24 +0200

    Update K6SoakTest.js

--- Tests/K6SoakTest.js
+++ Tests/K6SoakTest.js
@@ -1,5 +1,5 @@
 import http from 'k6/http'
-import { check } from 'k6';
+import { check, fail } from 'k6';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
